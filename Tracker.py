a367b163824a7339d12f73c58a7b0ca83d5726fe
Shreyas Jaiswal <jaiswalshreyas87@gmail.com>
2025-08-24 12:45:22 +0530

    Uploading main files
    adding the main code

---
+++ Tracker.py
@@ -0,0 +1,72 @@
+# importinng important libs
+import csv
+import os
+from datetime import datetime
+#class 
+class ExpenseTracker:
+    def __init__(self, filename="file.csv"):
+        self.filename = filename
+
+    def addnew(self,amount,category,date,note): #appends a list of values to the file
+        lis=[amount,category,date,note] 
+        file_exists = os.path.isfile(self.filename) #checks if the file exists 
+        with open(self.filename, "a", newline="") as f: #opens the file with append mode named as f
+                writer = csv.writer(f)#writing the values
+                if not file_exists: 
+                    writer.writerow(["Amount", "Category", "Date", "Note"]) #writes the name
+                writer.writerow(lis) 
+
+    def showexp(self):
+        counter=0
+        with open(self.filename, "r", newline="") as f:
+            read=csv.reader(f)
+            next(read)
+            for row in read:
+                 counter+=1
+                 print("#########################")
+                 print(f"data number.{counter}")
+                 print("#########################")
+                 print(f"amount={row[0]}")
+                 print(f"category={row[1]}")
+                 print(f"date={row[2]}")
+                 print(f"note={row[3]}\n")
+
+    def totalsp(self):
+         with open(self.filename, "r", newline="") as f:
+            read=csv.reader(f)
+            total=0
+            for row in read:
+                if row[0].isdigit(): # this skips the header
+                    total+=int(row[0]) #calculates the tota;
+            print(f"the total expense of all time is Rs. {total}")
+
+    def monthlysp(self,year,month):
+        with open(self.filename, "r", newline="") as f:
+            read=csv.reader(f)
+            total=0
+            for row in read:
+                if not row or row[0] == "Amount": #skips empty and header rows
+                    continue
+                try:
+                    d = datetime.strptime(row[2], "%Y-%m-%d").date()#conversion into date time format
+                    if d.year == year and d.month == month:
+                        total += int(row[0])
+                except Exception:
+                    pass
+            print(f"total expense of {year}-{month:02d} is Rs.{total} ")
+    def flt_ctg(self,category): #filtering by category
+        self.category=category
+        counter=0
+        with open(self.filename,"r",newline="") as f:
+            read=csv.reader(f)
+            next(read)
+            for row in read:
+                if row[1]==self.category:
+                    counter+=1
+                print("#########################")
+                print(f"data number.{counter}")
+                print("#########################")
+                print(f"amount={row[0]}")
+                print(f"category={row[1]}")
+                print(f"date={row[2]}")
+                print(f"note={row[3]}\n")
---
+++ project.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+from Tracker import ExpenseTracker
+#this takes the user input so we don't have to validate the input each time
+def gtin(inpm,cast_fun=str,condition=lambda x:True,er_msg="Invalid try again"):
+    '''inpm takes the input message, cast_fun takes the data type you want your data to be
+    condition parameter takes a condition and then error message desplays the error message'''
+    while True:
+        try:
+            value=cast_fun(input(inpm))
+            if not condition(value):
+                print(er_msg)
+                continue
+            return value
+        except ValueError:
+            pass
+#this is so the user can exit the program
+def endloop():
+    a=input("add more ? (y/n) >>")
+    try:
+        if a=="n":
+            return "y"
+        elif a=="y":
+            return "n"
+    except (TypeError,ValueError) as e:
+        print(f"try again..!!its an {e}error")
+#just to make the category selection easier
+def CoE(category):
+        if category==1:
+            return "FOOD"
+        elif category==2:
+            return "TRAVEL"
+        elif category==3:
+            return "STUDIES"
+        elif category==4:
+            return "MEDICAL"
+        elif category==5:
+            return "PERSONAL"
+        elif category==6:
+            return "OTHERS"
+
+
+l=ExpenseTracker()
+
+ans ="n"
+while ans!= "y": 
+    #menu
+    print("choose your num\n""1. add\n""2.Show expense\n""3.TOTAL\n""4.Monthly spent\n""5.search by cetegory\n""6.exit\n")
+    try:
+        a=int(input("your choice.. =")) #selection for the menu
+    except ValueError:
+         print("enter 1-5 only") #error handling incase user inputs incorrect value
+         continue
+    if a==1 :
+            amount=gtin("Enter Amount=>",cast_fun=int,er_msg="it should be a number") #using the gtin function
+            print("1.Food")
+            print("2.Travel")
+            print("3.Studies")
+            print("4.Medical")
+            print("5.Personal")
+            print("6.Others")
+            cat=gtin("Enter Category=>",cast_fun=int,condition=lambda x:x>0 and x<6,er_msg="it should be a number only 1-6")
+            c=CoE(category=cat)
+            valid_date= gtin("Enter date (YYYY-MM-DD): ",cast_fun=lambda x: datetime.strptime(x,"%Y-%m-%d").date(),er_msg="Invalid date format! Please use YYYY-MM-DD.")
+            note=input("enter a note if you want, else type none-")
+            l.addnew(amount,c,valid_date,note) #adding the new entries to the csv file
+            ans=endloop()
+    elif a==2:
+        l.showexp()
+        ans=endloop()
+    elif a==3:
+            l.totalsp()
+            ans=endloop()
+    elif a==4:
+        try:    
+            year=int(input("enter the year="))
+            month=int(input("enter the month(01-12)="))
+            l.monthlysp(year,month)
+            ans=endloop()
+        except Exception:
+            continue
+    elif a==5:
+        #menu
+        print("******************************\n")
+        print("1.Food")
+        print("2.Travel")
+        print("3.Studies")
+        print("4.Medical")
+        print("5.Personal")
+        print("6.Others")
+        cate_n=gtin("enter a valid category",int,condition=lambda x: 1 <= x <= 6,er_msg="invalid category(1-6 only)")
+        cate=CoE(cate_n)
+        l.flt_ctg(cate)
+        ans=endloop()
+    elif a==6:
+         break
+    else:
+         print("try again")
+print("Thank you for using :3")#end
